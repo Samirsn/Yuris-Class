37031941740fb35a786949cc6447d28f6a7e5302
Samir <samirnaimi@gmail.com>
2014-03-17 23:02:45 -0400

    Advanced Physics Assignment

---
+++ CatNap2/CatNap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CatNap
+//
+//  Created by Samir Naimi on 3/15/14.
+//  Copyright (c) 2014 Samir Naimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
